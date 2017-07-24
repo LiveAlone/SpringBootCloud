3ce875507d4199ef7f32b179908050c081307faa
yaoqijun <yaoqijunmail@gmail.com>
2017-07-25 00:58:25 +0800

    no message

--- service-client/src/main/java/org/yqj/service/demo/BootDemoApplication.java
+++ service-client/src/main/java/org/yqj/service/demo/BootDemoApplication.java
@@ -1,8 +1,11 @@
 package org.yqj.service.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Created by yaoqijun.
@@ -18,5 +21,14 @@ public class BootDemoApplication {
                 .banner(new DemoBanner())
                 .sources(BootDemoApplication.class)
                 .run(args);
+    }
+
+    @Bean
+    public CommandLineRunner runner(DiscoveryClient discoveryClient){
+        return args ->
+            discoveryClient.getInstances("SERVICE-HI")
+                    .forEach(serviceInstance -> System.out.println(
+                            String.format("Found %s %s:%s", serviceInstance.getServiceId(), serviceInstance.getHost(), serviceInstance.getPort())));
+
     }
 }
